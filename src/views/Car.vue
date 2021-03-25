5b6996bfe14dd46d4e9799401af035fb9c2eabb5
Mr <37304121+IRHM@users.noreply.github.com>
2021-03-25 19:18:34 +0000

    Create basic car page

--- src/views/Car.vue
+++ src/views/Car.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>
+  <div class="box">
     <h1 class="title">{{ currentCar.make }} {{ currentCar.model }}</h1>
+
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Price</p>
+          <p class="title">${{ currentCar.price }}</p>
+        </div>
+      </div>
+
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Transmission</p>
+          <p class="title">{{ currentCar.transmission }}</p>
+        </div>
+      </div>
+
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Body Type</p>
+          <p class="title">{{ currentCar.bodyType }}</p>
+        </div>
+      </div>
+
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Doors</p>
+          <p class="title">{{ currentCar.doors }}</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Fuel</p>
+          <p class="title">{{ currentCar.fuel }}</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Mileage</p>
+          <div>
+            <span class="title">{{ currentCar.mileage }}</span>
+            <span class="subtitle">MI</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <figure class="image">
+      <img :src="currentCar.image" alt="" style="object-fit: cover;" />
+    </figure>
   </div>
 </template>
 
